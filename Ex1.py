b3b4ffd2421c754c77f3bbea43ce8a0ae945b666
MattDff <matt.doffe@gmail.com>
2025-11-26 08:39:52 +0100

    Ex1 1.2.2

---
+++ Ex1.py
@@ -0,0 +1,19 @@
+from Crypto.Hash import SHA1, MD5
+
+hash1 = SHA1.new(data=b'ENSEA')
+hash2 = SHA1.new(data=b'eNSeA')
+hash3 = SHA1.new(data=b'ENSEA')
+hash4 = SHA1.new(data=b'EN5EA')
+hash5 = MD5.new(data=b'ENSEA')
+hash6 = MD5.new(data=b'eNSeA')
+hash7 = MD5.new(data=b'ENSEA')
+hash8 = MD5.new(data=b'EN5EA')
+
+print("SHA1 de ENSEA : ",hash1.hexdigest())
+print("SHA1 de eNSEA : ",hash2.hexdigest())
+print("SHA1 de eNSeA : ",hash3.hexdigest())
+print("SHA1 de EN5EA : ",hash4.hexdigest())
+print("MD5 de ENSEA : ",hash5.hexdigest())
+print("MD5 de eNSEA : ",hash6.hexdigest())
+print("MD5 de eNSeA : ",hash7.hexdigest())
+print("MD5 de EN5EA : ",hash8.hexdigest())
